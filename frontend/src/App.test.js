1c3458941cb0d61985e642497a1a64d873d044ee
baldm <jonathan@zoern.dk>
2022-08-30 12:40:11 +0200

    Tilføjet login knap og lavet unittest

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link and login button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn DevOps/i);
   expect(linkElement).toBeInTheDocument();
+  let button = screen.getByRole("button");
+  expect(button).toHaveTextContent("Login");
 });
+
