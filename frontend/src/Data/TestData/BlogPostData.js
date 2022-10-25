7e33b7db5d3ede1ca71f12e8703dd58c2c84790b
baldm <jonathan@zoern.dk>
2022-10-25 13:57:36 +0200

    Changed some dummy data to test things

--- frontend/src/Data/TestData/BlogPostData.js
+++ frontend/src/Data/TestData/BlogPostData.js
@@ -36,7 +36,7 @@ export const BlogPostData = [
 
   {
     title: "GOOGL",
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five.....",
+    body: "Lorem Ipsum dolor is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five.....",
     author: "John Doesen",
     liked: false,
     dislike: false,
