69d0462c450a1faa925361781762152ff232250e
louis <louis.mail.dk@gmail.com>
2022-11-02 18:01:29 +0100

    onclick login

--- frontend/src/view/AccountView.jsx
+++ frontend/src/view/AccountView.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+//fra øvelse 9 
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  //name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+const token = getParameterByName("token");
+if (token!=null && token.length>0){
+  //Store token and redirect to baseURL
+  localStorage.setItem("portal-jwt-Token",token);
+  window.location.replace("/");
+}
+
 export const AccountView = () => {
   return (
     <div className="h-[400px] w-full shadow rounded-lg bg-white mb-5 md:mb-10">
@@ -10,7 +27,7 @@ export const AccountView = () => {
       </div>
       <button
         className="w-full h-1/5 bg-[#7382D9]  rounded-b-lg text-white p-2 font-extrabold text-xl"
-        onClick={() => console.log("Login")}
+        onClick={getParameterByName("/login","localhost:8000")}
       >
         Login med CampusNet
       </button>
