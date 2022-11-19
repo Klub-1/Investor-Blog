03ad286a2e8fc3406817a1f3caa48b38927e5e6a
baldm <jonathan@zoern.dk>
2022-11-19 12:13:34 +0100

    Test for creating user and post

--- backend/tests/endpoint_test.py
+++ backend/tests/endpoint_test.py
@@ -2,6 +2,7 @@
    on: https://github.com/baldm/TestLens/blob/main/tests/backend_test.py
 """
 
+from random import randint
 import unittest
 import requests
 
@@ -13,27 +14,41 @@ class TestAPI(unittest.TestCase):
         self.api_session = requests.Session()
 
     def test_create_user_and_delete(self):
-
+        random_user = randint(100000, 999999)
         new_user = {
-            "id": "s123456",
-            "username": "test_user",
-            "email": "s123456@student.dtu.dk",
+            "id": f"s{random_user}",
+            "username": f"test_user_{random_user}",
+            "email": f"s{random_user}@student.dtu.dk",
             "blogposts": []
         }
 
         # Test creating new user
-        res = self.api_session.post(self.url + 'users/', data=new_user)
-        self.assertEqual(res.status_code, 201)
-
-        new_user_url = res.json()['url']
-
-        # Test the new user exists
-        res = self.api_session.get(new_user_url)
+        res = self.api_session.post(self.url + 'users/', json=new_user)
         self.assertEqual(res.status_code, 200)
 
-        # Remove the user again
-        res = self.api_session.delete(new_user_url)
-        self.assertEqual(res.status_code, 204)
+        new_user_id = res.json()['id']
+
+        # Test the new user exists
+        res = self.api_session.get(self.url + f'users/{new_user_id}')
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_blogpost(self):
+
+        # Get a random user
+        res = self.api_session.get(self.url + 'users/')
+        user_id = res.json()[0]['id']
+
+        new_post = {
+            "title": "test title",
+            "content": "test content",
+            "likes": randint(1, 12),
+            "dislikes": randint(0, 12)
+        }
+
+        # Send request to create post
+        request_url = self.url + f'users/{user_id}/blogposts/'
+        res = self.api_session.post(request_url, json=new_post)
+        self.assertEqual(res.status_code, 200)
 
     def tearDown(self):
         self.api_session.close()
