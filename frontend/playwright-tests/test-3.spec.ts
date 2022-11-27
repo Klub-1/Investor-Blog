7c30a85ee145e9b99b87db9a02535de2a662e8bd
louismonty <louis.mail.dk@gmail.com>
2022-11-27 19:04:33 +0100

    playwright test with login

---
+++ frontend/playwright-tests/test-3.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  await page.goto('http://localhost:3000/');
+
+  await page.getByRole('link').nth(4).click();
+  await expect(page).toHaveURL('http://localhost:3000/account');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await page.getByRole('button', { name: 'Registrer' }).click();
+
+  await page.getByPlaceholder('Brugernavn').click();
+
+  await page.getByPlaceholder('Brugernavn').fill('test');
+
+  await page.getByPlaceholder('Brugernavn').press('Tab');
+
+  await page.getByPlaceholder('Email').fill('test');
+
+  await page.getByPlaceholder('Email').press('Tab');
+
+  await page.getByPlaceholder('Password').fill('test');
+
+  await page.getByRole('button', { name: 'Opret konto' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/');
+  await new Promise(f => setTimeout(f, 1000));
+
+  await page.locator('a:nth-child(4)').click();
+  await expect(page).toHaveURL('http://localhost:3000/stocks');
+
+  await page.getByPlaceholder('Søg efter aktie...').click();
+
+  await page.getByPlaceholder('Søg efter aktie...').fill('APLE');
+
+  await page.getByRole('button').click();
+
+  await page.getByRole('button').nth(1).click();
+
+  await page.getByPlaceholder('Søg efter aktie...').click();
+
+  await page.getByPlaceholder('Søg efter aktie...').fill('');
+
+  await page.locator('#root div:has-text("Søg efter aktie")').nth(1).click();
+
+  await page.locator('.rounded-full').first().click();
+  await expect(page).toHaveURL('http://localhost:3000/');
+
+  await page.locator('a:nth-child(4)').click();
+  await expect(page).toHaveURL('http://localhost:3000//stocks');
+
+  await page.getByRole('heading', { name: 'APLE' }).click();
+
+  await page.getByRole('button').nth(1).click();
+
+});
